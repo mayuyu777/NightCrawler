ac07bd371febc9270747a6721653fa472260aad3
natto777 <72873081+natto777@users.noreply.github.com>
2021-06-02 04:03:57 -0700

    minor

--- NightCrawler/Assets/Chalice.cs
+++ NightCrawler/Assets/Chalice.cs
@@ -6,10 +6,14 @@ using UnityEngine.SceneManagement;
 public class Chalice : MonoBehaviour
 {
     public GameObject youwin;
+    public AudioClip effect;
+    public AudioSource audioSource;
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            audioSource.clip = effect;
+            audioSource.Play();
             StartCoroutine(Finish());
         }
     }
